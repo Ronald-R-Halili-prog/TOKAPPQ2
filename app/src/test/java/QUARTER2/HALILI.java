f3da79658c0dbe941b5ea5624fb967e23107f8f6
Ronald-R-Halili-prog <ronaldhalili@gmail.com>
2026-09-16 22:41:48 +0800

    Added Halili's profile test

--- app/src/test/java/QUARTER2/HALILI.java
+++ app/src/test/java/QUARTER2/HALILI.java
@@ -11,6 +11,6 @@ public class HALILI {
 
         System.out.println("---MY DIGITAL PROFILE---");
         System.out.println("Hello, my name is" + myName + "and I am" + myAge + "years old");
-        System.out.println("I have one pet named" + petname);
-        System.out.println("If i could eat" + favfood + "everyday!");}
+        System.out.println("I have a wonderful pet named" + petname + ".");
+        System.out.println("If i could eat, I would eat" + favfood + "everyday!");}
     }
