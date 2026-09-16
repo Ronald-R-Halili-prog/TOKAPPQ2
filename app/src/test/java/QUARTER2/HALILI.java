11a4c5f61fb32d908577397f36744ba59a93527e
Ronald-R-Halili-prog <ronaldhalili@gmail.com>
2026-09-16 22:21:36 +0800

    Added Halili's profile test

--- app/src/test/java/QUARTER2/HALILI.java
+++ app/src/test/java/QUARTER2/HALILI.java
@@ -1,4 +1,16 @@
-package com.example.tokappq2;
-
+package QUARTER2;
+import org.junit.Test;
 public class HALILI {
-}
+
+    @Test
+    public void printMYprofile() {
+        String myName = "HALILI";
+        String petname = "Cookie";
+        String favfood = "Adobo";
+        int myAge = 17;
+
+        System.out.println("---MY DIGITAL PROFILE---");
+        System.out.println("Hello, my name is" + myName + "and I am" + myAge + "years old");
+        System.out.println("I have one pet named" + petname);
+        System.out.println("If i could eat" + favfood + "everyday!");}
+    }
