5b8d78e04df71dba9a782d8b081a40b3b81a8fec
aljrk3n <aljr.kn@gmail.com>
2026-09-17 19:02:53 +0800

    done for mini peta  ANTONIO.java

--- app/src/test/java/QUARTER2/ANTONIO.java
+++ app/src/test/java/QUARTER2/ANTONIO.java
@@ -1,4 +1,16 @@
 package QUARTER2;
-
+import org.junit.Test;
 public class ANTONIO {
+
+    @Test
+    public void printMYprofile() {
+        String myName = "ANTONIO";
+        String petname = "BLACKY";
+        String favfood = "ANYTHING";
+        int myAge = 17;
+
+        System.out.println("---MY DIGITAL PROFILE---");
+        System.out.println("Hello, my name is" + myName + "and I am" + myAge + "years old");
+        System.out.println("It's black, that's my pet color" + petname + "BLACKY");
+        System.out.println("Everything tastes better when you're hungry" + favfood + "YUMMY!!");}
 }
