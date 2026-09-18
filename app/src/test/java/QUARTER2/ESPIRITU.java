0ae15f0c02bd68bb2d0e8cf131a2a6c47d3412cc
Ronald-R-Halili-prog <ronaldhalili@gmail.com>
2026-09-18 21:35:58 +0800

    Added ESPIRITU's profile test

--- app/src/test/java/QUARTER2/ESPIRITU.java
+++ app/src/test/java/QUARTER2/ESPIRITU.java
@@ -1,4 +1,16 @@
 package QUARTER2;
-
+import org.junit.Test;
 public class ESPIRITU {
-}
+
+    @Test
+    public void printMyprofile() {
+        String myName = "ESPIRITU";
+        String petname = "Ziii";
+        String favfood = "Sinigang";
+        int myAge = 17;
+
+        System.out.println("---MY DIGITAL PROFILE---");
+        System.out.println("Hello, my name is" + myName + "and I am" + myAge + "years old");
+        System.out.println("I have a wonderful pet named" + petname + ".");
+        System.out.println("If i could eat, I would eat" + favfood + "everyday!");}
+    }
