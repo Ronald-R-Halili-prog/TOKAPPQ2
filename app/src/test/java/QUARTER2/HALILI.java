7bf77ee7ce0ec5c8b87058008fd49dac40d1a3a1
Ronald-R-Halili-prog <ronaldhalili@gmail.com>
2026-09-18 21:35:32 +0800

    Added Halili's profile test

--- app/src/test/java/QUARTER2/HALILI.java
+++ app/src/test/java/QUARTER2/HALILI.java
@@ -3,14 +3,14 @@ import org.junit.Test;
 public class HALILI {
 
     @Test
-    public void printMYprofile() {
+    public void printMyprofile() {
         String myName = "HALILI";
         String petname = "Cookie";
         String favfood = "Adobo";
         int myAge = 17;
 
-        System.out.println("---MY DIGITAL PROFILE---");
-        System.out.println("Hello, my name is" + myName + "and I am" + myAge + "years old");
-        System.out.println("I have a wonderful pet named" + petname + ".");
-        System.out.println("If i could eat, I would eat" + favfood + "everyday!");}
+        System.out.println ("---MY DIGITAL PROFILE---");
+        System.out.println ("Hello, my name is" + myName + "and I am" + myAge + "years old");
+        System.out.println ("I have a wonderful pet named" + petname + ".");
+        System.out.println ("If i could eat, I would eat" + favfood + "everyday!");}
     }
